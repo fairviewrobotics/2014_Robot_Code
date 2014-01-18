9846e1c0e378f755c8c0fc89db5a30b201eb53e3
programming team <jasonvperrin@gmail.com>
2014-01-18 11:46:37 -0700

    Jason loves indentation too much

--- BuiltinDefaultCode/BuiltinDefaultCode.cpp
+++ BuiltinDefaultCode/BuiltinDefaultCode.cpp
@@ -42,165 +42,164 @@ class BuiltinDefaultCode : public IterativeRobot {
 	AxisCamera *camera;
 
 public:
-/**
- * Constructor for this "BuiltinDefaultCode" Class.
- * 
- * The constructor creates all of the objects used for the different inputs and outputs of
- * the robot.  Essentially, the constructor defines the input/output mapping for the robot,
- * providing named objects for each of the robot interfaces. 
- */
-        BuiltinDefaultCode(void) {
-                printf("BuiltinDefaultCode Constructor Started\n");
-                left_1  = new Victor(1);
-                left_2  = new Victor(2);
-                right_1 = new Victor(3);
-                right_2 = new Victor(4);
-                shiftRight = new Solenoid(1);
-                shiftLeft  = new Solenoid(2);
-                mainGyro = new Gyro(5);
-                leftEncoder    = new Encoder(6, 8); // Second int is a placeholder to fix an error with the code (Encoder takes 2 ints)
-                rightEncoder   = new Encoder(7, 9); // Same here
-                shooterEncoder = new Encoder(10, 11); // Also same here
-                // Create a robot using standard right/left robot drive on PWMS 1, 2, 3, and #4
-                m_robotDrive = new RobotDrive(1, 2, 3, 4);
-                gamePad = new Joystick(1);
+	/**
+	 * Constructor for this "BuiltinDefaultCode" Class.
+	 * 
+	 * The constructor creates all of the objects used for the different inputs and outputs of
+	 * the robot.  Essentially, the constructor defines the input/output mapping for the robot,
+	 * providing named objects for each of the robot interfaces. 
+	 */
+	BuiltinDefaultCode(void) {
+		printf("BuiltinDefaultCode Constructor Started\n");
+		left_1  = new Victor(1);
+		left_2  = new Victor(2);
+		right_1 = new Victor(3);
+		right_2 = new Victor(4);
+		shiftRight = new Solenoid(1);
+		shiftLeft  = new Solenoid(2);
+		mainGyro = new Gyro(5);
+		leftEncoder    = new Encoder(6, 8); // Second int is a placeholder to fix an error with the code (Encoder takes 2 ints)
+		rightEncoder   = new Encoder(7, 9); // Same here
+		shooterEncoder = new Encoder(10, 11); // Also same here
+		// Create a robot using standard right/left robot drive on PWMS 1, 2, 3, and #4
+		m_robotDrive = new RobotDrive(1, 2, 3, 4);
+		gamePad = new Joystick(1);
 
-                // Acquire the Driver Station object
-                m_ds = DriverStation::GetInstance();
-                m_priorPacketNumber = 0;
-                m_dsPacketsReceivedInCurrentSecond = 0;
+		// Acquire the Driver Station object
+		m_ds = DriverStation::GetInstance();
+		m_priorPacketNumber = 0;
+		m_dsPacketsReceivedInCurrentSecond = 0;
 
-                // Initialize counters to record the number of loops completed in autonomous and teleop modes
-                m_autoPeriodicLoops = 0;
-                m_disabledPeriodicLoops = 0;
-                m_telePeriodicLoops = 0;
+		// Initialize counters to record the number of loops completed in autonomous and teleop modes
+		m_autoPeriodicLoops = 0;
+		m_disabledPeriodicLoops = 0;
+		m_telePeriodicLoops = 0;
 
-                printf("BuiltinDefaultCode Constructor Completed\n");
-        }
-        
-        
-        /********************************** Init Routines *************************************/
+		printf("BuiltinDefaultCode Constructor Completed\n");
+	}
+	
+	
+	/********************************** Init Routines *************************************/
 
 
-        void RobotInit(void) {
-                // Actions which would be performed once (and only once) upon initialization of the
-                // robot would be put here.
-                shiftLeft->Set(false); //low gear
-                shiftRight->Set(false); //low gear
-                passingPiston->Set(false); //retracted
-                printf("RobotInit() completed.\n");
+	void RobotInit(void) {
+		// Actions which would be performed once (and only once) upon initialization of the
+		// robot would be put here.
+		shiftLeft->Set(false); //low gear
+		shiftRight->Set(false); //low gear
+		passingPiston->Set(false); //retracted
+		printf("RobotInit() completed.\n");
+	}
+	
+	void DisabledInit(void) {
+		m_disabledPeriodicLoops = 0; // Reset the loop counter for disabled mode
+	}
 
-        }
-        
-        void DisabledInit(void) {
-                m_disabledPeriodicLoops = 0; // Reset the loop counter for disabled mode
-        }
+	void AutonomousInit(void) {
+		m_autoPeriodicLoops = 0; // Reset the loop counter for autonomous mode
+		mainGyro->Reset();       // Assures us gyro starts at 0 degrees
+	}
 
-        void AutonomousInit(void) {
-                m_autoPeriodicLoops = 0; // Reset the loop counter for autonomous mode
-                mainGyro->Reset();       // Assures us gyro starts at 0 degrees
-        }
+	void TeleopInit(void) {
+		m_telePeriodicLoops = 0; // Reset the loop counter for teleop mode
+		m_dsPacketsReceivedInCurrentSecond = 0; // Reset the number of dsPackets in current second
+	}
 
-        void TeleopInit(void) {
-                m_telePeriodicLoops = 0; // Reset the loop counter for teleop mode
-                m_dsPacketsReceivedInCurrentSecond = 0; // Reset the number of dsPackets in current second
-        }
+	/********************************** Periodic Routines *************************************/
 
-        /********************************** Periodic Routines *************************************/
+	void DisabledPeriodic(void) {
+	}
 
-        void DisabledPeriodic(void) {
-        }
+	void AutonomousPeriodic(void) 
+	{
+		initialShot(1); // 1 is a temporary value.
+		findBall();
+		seekAndDestroy();
+		reposition();
+		shoot();
+		findBall();
+		seekAndDestroy();
+		reposition();
+		shoot();
+	}
 
-        void AutonomousPeriodic(void) 
-        {
-                initialShot(1); // 1 is a temporary value.
-                findBall();
-                seekAndDestroy();
-                reposition();
-                shoot();
-                findBall();
-                seekAndDestroy();
-                reposition();
-                shoot();
-        }
+	/********************************* Teleop methods *****************************************/
+	void motorControlLeft(float speed) 
+	{
+		left_1->SetSpeed(speed);
+		left_2->SetSpeed(speed);
+	}
 
-        /********************************* Teleop methods *****************************************/
-        void motorControlLeft(float speed) 
-        {
-                left_1->SetSpeed(speed);
-                left_2->SetSpeed(speed);
-        }
+	void motorControlRight(float speed)
+	{
+		right_1->SetSpeed(speed);
+		right_2->SetSpeed(speed);
+	}
 
-        void motorControlRight(float speed)
-        {
-                right_1->SetSpeed(speed);
-                right_2->SetSpeed(speed);
-        }
+	void ShiftHigh(void) 
+	{
+		// shiftRight->Get() return value: false is low gear, true is high gear
+		if(!(shiftRight->Get())) 
+		{
+			shiftRight->Set(true);
+			shiftLeft->Set(true);
+		}
+	}
 
-        void ShiftHigh(void) 
-        {
-                // shiftRight->Get() return value: false is low gear, true is high gear
-                if(!(shiftRight->Get())) 
-                {
-                        shiftRight->Set(true);
-                        shiftLeft->Set(true);
-                }
-        }
+	void ShiftLow(void) 
+	{
+		// shiftRight->Get() return value: false is low gear, true is high gear
+		if(shiftRight->Get()) {
+			shiftRight->Set(false);
+			shiftLeft->Set(false);
+		}
+	}
 
-        void ShiftLow(void) 
-        {
-                // shiftRight->Get() return value: false is low gear, true is high gear
-                if(shiftRight->Get()) {
-                        shiftRight->Set(false);
-                        shiftLeft->Set(false);
-                }
-        }
+	void TeleopPeriodic(void)
+	{
+		bool flag = true; // Flag object initial declaration to ensure passing piston toggle works properly
 
-        void TeleopPeriodic(void)
-        {
-                bool flag = true; // Flag object initial declaration to ensure passing piston toggle works properly
+		float leftStick  = gamePad->GetRawAxis(4);
+		float rightStick = gamePad->GetRawAxis(2);
+		bool rightBumper = gamePad->GetRawButton(6);
+		bool leftBumper  = gamePad->GetRawButton(5);
+		bool buttonA     = gamePad->GetRawButton(2);
 
-                float leftStick  = gamePad->GetRawAxis(4);
-                float rightStick = gamePad->GetRawAxis(2);
-                bool rightBumper = gamePad->GetRawButton(6);
-                bool leftBumper  = gamePad->GetRawButton(5);
-                bool buttonA     = gamePad->GetRawButton(2);
-
-                if(fabs(leftStick) >= 0.05 || fabs(rightStick >= 0.05)) 
-                        {
-                                m_robotDrive->TankDrive(leftStick, rightStick);
-                        }
-                else if(rightBumper || leftBumper) 
-                        {
-                                if(rightBumper && !leftBumper) 
-                                {
-                                        ShiftHigh();
-                                }
-                                else if(leftBumper && !rightBumper) 
-                                {
-                                        ShiftLow();
-                            }
-                        }
-                else if(buttonA && flag)
-                        {
-                                flag = false;
-                                passingPiston->Set(true);
-                        }
-                 else
-                         {
-                                if(!buttonA)
-                                {
-                                        flag = false;
-                                        motorControlLeft(0.0);
-                                        motorControlRight(0.0);
-                                }
-                                else
-                                {
-                                        motorControlLeft(0.0);
-                                        motorControlRight(0.0);
-                                }
-                        }
-        } 
+		if(fabs(leftStick) >= 0.05 || fabs(rightStick >= 0.05)) 
+		{
+			m_robotDrive->TankDrive(leftStick, rightStick);
+		}
+		else if(rightBumper || leftBumper) 
+		{
+			if(rightBumper && !leftBumper) 
+			{
+				ShiftHigh();
+			}
+			else if(leftBumper && !rightBumper) 
+			{
+				ShiftLow();
+			}
+		}
+		else if(buttonA && flag)
+		{
+			flag = false;
+			passingPiston->Set(true);
+		}
+		else
+		{
+			if(!buttonA)
+			{
+				flag = false;
+				motorControlLeft(0.0);
+				motorControlRight(0.0);
+			}
+			else
+			{
+				motorControlLeft(0.0);
+				motorControlRight(0.0);
+			}
+		}
+	} 
 
 
 	/********************************** Continuous Routines *************************************/
@@ -267,7 +266,7 @@ public:
 		}
 		else
 		{
-			while(mainGyro->GetAngle() < x) // Encoder -> turn radius shit here check with gyro 
+			while(mainGyro->GetAngle() < x) // Encoder -> turn radius shit here check with gyro
 			{
 				motorControlLeft(0.5);
 				motorControlRight(-0.5);
@@ -278,18 +277,17 @@ public:
 	void DisabledContinuous(void)
 	{
 
-
-        }
+	}
         
-        void AutonomousContinuous(void)        
-        {
+	void AutonomousContinuous(void)        
+	{
 
-        }
+	}
 
-        void TeleopContinuous(void)
-        {
-                
-        }
+	void TeleopContinuous(void)
+	{
+			
+	}
 };
 
 START_ROBOT_CLASS(BuiltinDefaultCode);
