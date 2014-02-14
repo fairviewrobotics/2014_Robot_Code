dd7c9014446ac719c0626aff8a117c92c4cd4930
Gordon Gu <gordonsgu7@gmail.com>
2014-02-14 15:22:35 -0700

    Improved autonomous code

--- BuiltinDefaultCode/BuiltinDefaultCode.cpp
+++ BuiltinDefaultCode/BuiltinDefaultCode.cpp
@@ -325,67 +325,121 @@ public:
 
 	int identifyBall(void) {
 
-//		Threshold threshold(60, 100, 90, 255, 20, 255);	//HSV threshold criteria, ranges are in that order ie. Hue is 60-100
-//		ParticleFilterCriteria2 criteria[] = {
-//				{IMAQ_MT_AREA, AREA_MINIMUM, 65535, false, false}
-//		}; //Particle filter criteria, used to filter out small particles
-		// AxisCamera &camera = AxisCamera::GetInstance();	//To use the Axis camera uncomment this line
+		while (IsAutonomous() && IsEnabled()) {
+            /**
+             * Do the image capture with the camera and apply the algorithm described above. This
+             * sample will either get images from the camera or from an image file stored in the top
+             * level directory in the flash memory on the cRIO. The file name in this case is "testImage.jpg"
+             */
+			ColorImage *image;
+			//image = new RGBImage("/testImage.jpg");		// get the sample image from the cRIO flash
 
-		/**
-		 * Do the image capture with the camera and apply the algorithm dscribed above. This
-		 * sample will either get images from the camera or from an image file stored in the top
-		 * level directory in the flash memory on the cRIO. The file name in this case is "testImage.jpg"
-		 */
+			image = camera.GetImage();				//To get the images from the camera comment the line above and uncomment this one
+			BinaryImage *thresholdImage = image->ThresholdHSV(threshold);	// get just the green target pixels
+			//thresholdImage->Write("/threshold.bmp");
+			BinaryImage *filteredImage = thresholdImage->ParticleFilter(criteria, 1);	//Remove small particles
+			//filteredImage->Write("Filtered.bmp");
 
-		// ColorImage *image;
-		//image = new RGBImage("/testImage.jpg");		// get the sample image from the cRIO flash
+			vector<ParticleAnalysisReport> *reports = filteredImage->GetOrderedParticleAnalysisReports();  //get a particle analysis report for each particle
 
-		// camera->GetImage(image);				//To get the images from the camera comment the line above and uncomment this one
-		// BinaryImage *thresholdImage = image->ThresholdHSV(threshold);	// get just the green target pixels
-		//thresholdImage->Write("/threshold.bmp");
-		// BinaryImage *convexHullImage = thresholdImage->ConvexHull(false);  // fill in partial and full rectangles
-		//convexHullImage->Write("/ConvexHull.bmp");
-		// BinaryImage *filteredImage = convexHullImage->ParticleFilter(criteria, 1);	//Remove small particles
-		//filteredImage->Write("Filtered.bmp");
+			verticalTargetCount = horizontalTargetCount = 0;
+			//Iterate through each particle, scoring it and determining whether it is a target or not
+			if(reports->size() > 0)
+			{
+				scores = new Scores[reports->size()];
+				for (unsigned int i = 0; i < MAX_PARTICLES && i < reports->size(); i++) {
+					ParticleAnalysisReport *report = &(reports->at(i));
 
-		// vector<ParticleAnalysisReport> *reports = filteredImage->GetOrderedParticleAnalysisReports();  //get a particle analysis report for each particle
-		// scores = new Scores[reports->size()];
+					//Score each particle on rectangularity and aspect ratio
+					scores[i].rectangularity = scoreCircularity(report);
+					scores[i].aspectRatioVertical = scoreAspectRatio(filteredImage, report, true);
+					scores[i].aspectRatioHorizontal = scoreAspectRatio(filteredImage, report, false);			
 
-		//Iterate through each particle, scoring it and determining whether it is a target or not
-//		for (unsigned i = 0; i < reports->size(); i++) {
-//			ParticleAnalysisReport *report = &(reports->at(i));
+					//Check if the particle is a horizontal target, if not, check if it's a vertical target
+					if(scoreCompare(scores[i], false))
+					{
+						printf("particle: %d  is a Horizontal Target centerX: %d  centerY: %d \n", i, report->center_mass_x, report->center_mass_y);
+						horizontalTargets[horizontalTargetCount++] = i; //Add particle to target array and increment count
+					} else if (scoreCompare(scores[i], true)) {
+						printf("particle: %d  is a Vertical Target centerX: %d  centerY: %d \n", i, report->center_mass_x, report->center_mass_y);
+						verticalTargets[verticalTargetCount++] = i;  //Add particle to target array and increment count
+					} else {
+						printf("particle: %d  is not a Target centerX: %d  centerY: %d \n", i, report->center_mass_x, report->center_mass_y);
+					}
+					printf("Scores rect: %f  ARvert: %f \n", scores[i].rectangularity, scores[i].aspectRatioVertical);
+					printf("ARhoriz: %f  \n", scores[i].aspectRatioHorizontal);	
+				}
 
-//				scores[i].rectangularity = scoreRectangularity(report);
-//				scores[i].aspectRatioOuter = scoreAspectRatio(filteredImage, report, true);
-//				scores[i].aspectRatioInner = scoreAspectRatio(filteredImage, report, false);			
-//				scores[i].xEdge = scoreXEdge(thresholdImage, report);
-//				scores[i].yEdge = scoreYEdge(thresholdImage, report);
+				//Zero out scores and set verticalIndex to first target in case there are no horizontal targets
+				target.totalScore = target.leftScore = target.rightScore = target.tapeWidthScore = target.verticalScore = 0;
+				target.verticalIndex = verticalTargets[0];
+				for (int i = 0; i < verticalTargetCount; i++)
+				{
+					ParticleAnalysisReport *verticalReport = &(reports->at(verticalTargets[i]));
+					for (int j = 0; j < horizontalTargetCount; j++)
+					{
+						ParticleAnalysisReport *horizontalReport = &(reports->at(horizontalTargets[j]));
+						double horizWidth, horizHeight, vertWidth, leftScore, rightScore, tapeWidthScore, verticalScore, total;
 
-//				if(scoreCompare(scores[i], false))
-//				{
-//					printf("particle: %d  is a High Goal  centerX: %f  centerY: %f \n", i, report->center_mass_x_normalized, report->center_mass_y_normalized);
-//					printf("Distance: %f \n", computeDistance(thresholdImage, report, false));
-//				} else if (scoreCompare(scores[i], true)) {
-//					printf("particle: %d  is a Middle Goal  centerX: %f  centerY: %f \n", i, report->center_mass_x_normalized, report->center_mass_y_normalized);
-//					printf("Distance: %f \n", computeDistance(thresholdImage, report, true));
-//				} else {
-//					printf("particle: %d  is not a goal  centerX: %f  centerY: %f \n", i, report->center_mass_x_normalized, report->center_mass_y_normalized);
-//				}
-			// printf("rect: %f  ARinner: %f \n", scores[i].rectangularity, scores[i].aspectRatioInner);
-			// printf("ARouter: %f  xEdge: %f  yEdge: %f  \n", scores[i].aspectRatioOuter, scores[i].xEdge, scores[i].yEdge);	
-//		}
-//		printf("\n");
-//		
-//		// be sure to delete images after using them
-//		delete filteredImage;
-//		delete convexHullImage;
-//		delete thresholdImage;
-//		delete image;
-//		
-//		//delete allocated reports and Scores objects also
-//		// delete scores;
-//		delete reports;
-		return 0;
+						//Measure equivalent rectangle sides for use in score calculation
+						imaqMeasureParticle(filteredImage->GetImaqImage(), horizontalReport->particleIndex, 0, IMAQ_MT_EQUIVALENT_RECT_LONG_SIDE, &horizWidth);
+						imaqMeasureParticle(filteredImage->GetImaqImage(), verticalReport->particleIndex, 0, IMAQ_MT_EQUIVALENT_RECT_SHORT_SIDE, &vertWidth);
+						imaqMeasureParticle(filteredImage->GetImaqImage(), horizontalReport->particleIndex, 0, IMAQ_MT_EQUIVALENT_RECT_SHORT_SIDE, &horizHeight);
+
+						//Determine if the horizontal target is in the expected location to the left of the vertical target
+						leftScore = ratioToScore(1.2*(verticalReport->boundingRect.left - horizontalReport->center_mass_x)/horizWidth);
+						//Determine if the horizontal target is in the expected location to the right of the  vertical target
+						rightScore = ratioToScore(1.2*(horizontalReport->center_mass_x - verticalReport->boundingRect.left - verticalReport->boundingRect.width)/horizWidth);
+						//Determine if the width of the tape on the two targets appears to be the same
+						tapeWidthScore = ratioToScore(vertWidth/horizHeight);
+						//Determine if the vertical location of the horizontal target appears to be correct
+						verticalScore = ratioToScore(1-(verticalReport->boundingRect.top - horizontalReport->center_mass_y)/(4*horizHeight));
+						total = leftScore > rightScore ? leftScore:rightScore;
+						total += tapeWidthScore + verticalScore;
+
+						//If the target is the best detected so far store the information about it
+						if(total > target.totalScore)
+						{
+							target.horizontalIndex = horizontalTargets[j];
+							target.verticalIndex = verticalTargets[i];
+							target.totalScore = total;
+							target.leftScore = leftScore;
+							target.rightScore = rightScore;
+							target.tapeWidthScore = tapeWidthScore;
+							target.verticalScore = verticalScore;
+						}
+					}
+					//Determine if the best target is a Hot target
+					target.Hot = hotOrNot(target);
+				}
+
+				if(verticalTargetCount > 0)
+				{
+					//Information about the target is contained in the "target" structure
+					//To get measurement information such as sizes or locations use the
+					//horizontal or vertical index to get the particle report as shown below
+					ParticleAnalysisReport *distanceReport = &(reports->at(target.verticalIndex));
+					double distance = computeDistance(filteredImage, distanceReport);
+					if(target.Hot)
+					{
+						printf("Hot target located \n");
+						printf("Distance: %f \n", distance);
+					} else {
+						printf("No hot target present \n");
+						printf("Distance: %f \n", distance);
+					}
+				}
+			}
+
+			// be sure to delete images after using them
+			delete filteredImage;
+			delete thresholdImage;
+			delete image;
+
+			//delete allocated reports and Scores objects also
+			delete scores;
+			delete reports;
+		}
 	}
 
 	void identifyHotSpots(void)
@@ -562,7 +616,7 @@ public:
 	double scoreCircularity(ParticleAnalysisReport *report)
 	{
 		if(report->boundingRect.width*report->boundingRect.height !=0){
-			return 100*report->particleArea/((report->boundingRect.width / 2) * (report->boundingRect.width / 2) * PI);
+			return 100*(report->boundingRect.width / 2) * (report->boundingRect.width / 2) * PI / ((report->boundingRect.width / 2) * (report->boundingRect.width / 2) * PI);
 		} else {
 			return 0;
 		}
